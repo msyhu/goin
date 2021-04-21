268b3a19a96f6d41e402cfe39f95afb85a3b17e7
msyhu <msyhu@korea.ac.kr>
2021-04-21 19:27:01 +0900

    module test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/msyhu/goin
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/msyhu/goin/upbit"
+)
+
+func main() {
+
+	//pricetype := "1"
+	//codes := []int{1,2}
+	upbit.Test()
+	fmt.Println("hello")
+}
