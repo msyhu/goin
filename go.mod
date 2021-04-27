4a1b2574537ed413ab25a19e7e5894d7b150b1a9
msyhu <msyhu@korea.ac.kr>
2021-04-28 00:56:25 +0900

    termui hello world

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/msyhu/goin
 
 go 1.16
 
-require github.com/gorilla/websocket v1.4.2
+require (
+	github.com/gizak/termui/v3 v3.1.0
+	github.com/gorilla/websocket v1.4.2
+)
--- main.go
+++ main.go
@@ -5,9 +5,28 @@
 package main
 
 import (
-	"github.com/msyhu/goin/upbit"
+	"log"
+
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
 )
 
 func main() {
-	upbit.UpbitWsClient()
+	//upbit.UpbitWsClient()
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
+	defer ui.Close()
+
+	p := widgets.NewParagraph()
+	p.Text = "Hello World!"
+	p.SetRect(0, 0, 25, 5)
+
+	ui.Render(p)
+
+	for e := range ui.PollEvents() {
+		if e.Type == ui.KeyboardEvent {
+			break
+		}
+	}
 }
